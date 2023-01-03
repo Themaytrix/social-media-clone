d0c2ec4eb49d7040fc7e7695a384b2e71dc6deaa
Themaytrix <obengraymond81@gmail.com>
2023-01-03 12:20:06 +0000

    ..

--- groups/models.py
+++ groups/models.py
@@ -3,6 +3,7 @@ from django.utils.text import slugify
 import misaka
 from django.contrib.auth import get_user_model
 from django import template
+from django.urls import reverse
 # returning the user model currently active
 
 User = get_user_model()
@@ -14,9 +15,38 @@ register = template.Library()
 
 # Create your models here.
 class Group(models.Model):
-    pass
-
-class GroupMembers(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    description = models.TextField(blank=True,default='')
+    description_html = models.TextField(editable=False,default='',blank=True)
+    members = models.ManyToManyField(User,through='GroupMember')
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args,**kwargs)
+    
+    # redirect after creating group
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+    
+    class Meta:
+        ordering = ['name']
+    
+    
+class GroupMember(models.Model):
+    # getting the group the user is in
     group = models.ForeignKey(Group,on_delete=models.CASCADE,related_name='membership')
-    user = models.ForeignKey(related_name='')
+    # getting the list of groups member is in
+    user = models.ForeignKey(related_name='user_groups')
+    
+    def __str__(self) -> str:
+        return self.user.username
+    
+    class Meta:
+        unique_together = ('group','user')
+    
     
--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.urls import reverse
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,DeleteView,UpdateView
+from .models import Group,GroupMember
 
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin,CreateView):
+    fields= ('name','description')
+    model = Group
